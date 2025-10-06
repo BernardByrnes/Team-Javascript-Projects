870fbc921ddb36210625729e3e6b04ebe9bc0394
Bernard Byrnes <bernardtamboedu@gmail.com>
2025-10-06 14:14:30 +0300

    Initial commit: Mini-Calendar project

---
+++ main.js
@@ -0,0 +1,23 @@
+const monthNameEl = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getMonth());
+
+// const month = date.getMonth();
+
+monthNameEl.textContent = date.toLocaleDateString("en", {
+  month: "long",
+});
+
+dayNameEl.textContent = date.toLocaleDateString("en", {
+  weekday: "long",
+});
+
+dayNumEl.textContent = date.getDate();
+yearEl.textContent = date.getFullYear();
